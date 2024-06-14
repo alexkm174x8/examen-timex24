00c384aaab38aad693900b2ee864cc13dff030c6
Alejandro Kong <115722702+alexkm174x8@users.noreply.github.com>
2024-06-14 11:56:30 -0600

    DONE

--- lib/jswatch_web/statecharts/indiglo_manager.ex
+++ lib/jswatch_web/statecharts/indiglo_manager.ex
@@ -29,18 +29,21 @@ defmodule JswatchWeb.IndigloManager do
     if snooze_timer != nil do
       Process.cancel_timer(snooze_timer)
     end
+    GenServer.cast(state.ui_pid, :unset_indiglo)
     {:noreply, %{state | st: AlarmOff, snooze_timer: nil}}
+  end
+
+  def handle_info(SnoozeActivated, %{ui_pid: pid, st: WaitingSnooze} = state) do
+    IO.puts("El botón inferior derecho fue presionado durante 2 segundos. Snooze activado.")
+    GenServer.cast(pid, :unset_indiglo)
+    :gproc.send({:p, :l, :ui_event}, :update_alarm)
+    {:noreply, %{state | st: IndigloOff}}
   end
 
   def handle_info(SnoozeActivated, %{ui_pid: pid, st: WaitingSnooze} = state) do
     GenServer.cast(pid, :unset_indiglo)
     :gproc.send({:p, :l, :ui_event}, :update_alarm)
     {:noreply, %{state | st: IndigloOff}}
-  end
-
-  def handle_info(Waiting_IndigloOff, %{ui_pid: pid, st: Waiting} = state) do
-    GenServer.cast(pid, :unset_indiglo)
-    {:noreply, %{state| st: IndigloOff}}
   end
 
   def handle_info(:start_alarm, %{ui_pid: pid, st: IndigloOff} = state) do
@@ -74,8 +77,8 @@ defmodule JswatchWeb.IndigloManager do
       GenServer.cast(pid, :unset_indiglo)
       {:noreply, %{state | count: 0, st: IndigloOff}}
     end
-
   end
+
   def handle_info(AlarmOff_AlarmOn, %{ui_pid: pid, count: count, st: AlarmOff} = state) do
     if count >= 1 do
       Process.send_after(self(), AlarmOn_AlarmOff, 500)
